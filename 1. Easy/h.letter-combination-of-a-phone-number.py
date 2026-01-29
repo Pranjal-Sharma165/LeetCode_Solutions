273840e493da144b384d0d7d67b20764a68d4f50
Pranjal Sharma <psharma165@my.bcit.ca>
2026-01-29 11:51:33 -0800

    find all the valid combinations

---
+++ 1. Easy/h.letter-combination-of-a-phone-number.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        numbers = {
+            "2" : "abc",
+            "3" : "def",
+            "4" : "ghi",
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv",
+            "9" : "wxyz"
+        }
+        result = []
+        def backtrack(index, path):
+            if index == len(digits):
+                result.append(path)
+                return
+            
+            digit = digits[index]
+            letters = numbers[digit]
+
+            for letter in letters:
+                backtrack(index + 1, path + letter)
+
+        backtrack(0, "")
+        return result
