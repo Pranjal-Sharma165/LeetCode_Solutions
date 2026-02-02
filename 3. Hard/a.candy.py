0b646bc6a0827fe086e5360bf44e29bcdb305f69
Pranjal Sharma <psharma165@my.bcit.ca>
2026-02-02 11:46:51 -0800

    Solve Candy problem using two-pass greedy approach

---
+++ 3. Hard/a.candy.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        if n == 0:
+            return 0
+
+        candies = [1] * n # Each child gets at least one candy
+
+        # Left to right pass
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        # Right to left pass
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
+        return sum(candies)
